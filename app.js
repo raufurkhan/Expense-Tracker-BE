db34c122d4fce34ddcc98045edefafea66c57b83
raufurkhan <raufurkhan82@gmail.com>
2024-08-03 00:37:31 +0530

    Daily App Expense Feature

--- app.js
+++ app.js
@@ -4,13 +4,16 @@ const app = express();
 const bodyParser = require("body-parser");
 const sequelize = require("./util/database");
 const userRouter = require("./router/userRouter");
-
+const expenseRouter = require("./router/expenseRouter");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use("/", userRouter);
 app.use("/user", userRouter);
+app.use("/homePage", expenseRouter);
+app.use("/expense", expenseRouter);
+
 
 sequelize
   .sync()
